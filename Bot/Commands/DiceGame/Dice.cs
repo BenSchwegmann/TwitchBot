4cc6998b378a1de6d758d10b36608267efde4540
benASTRO <ben.Socials@outlook.de>
2021-10-25 19:52:57 +0200

    Code cleaning

--- Bot/Commands/DiceGame/Dice.cs
+++ Bot/Commands/DiceGame/Dice.cs
@@ -15,22 +15,19 @@ namespace ApuDoingStuff.Commands.CommandClasses
             Random dice = new();
             int randDice = dice.Next(1, 7);
 
-
             if (database.Dicegamedbs.Any(d => d.UserName == chatMessage.Username))
             {
-                Console.WriteLine(chatMessage.Username);
                 database.Dicegamedbs.FirstOrDefault(d => d.UserName == chatMessage.Username).Points += randDice;
                 database.SaveChanges();
-                twitchBot.Send(chatMessage.Channel, $"/me APU {$"[ {database.Dicegamedbs.FirstOrDefault(r => r.UserName == chatMessage.Username).Rank} ]"} @{chatMessage.Username}, you got a {randDice} [current points: {database.Dicegamedbs.FirstOrDefault(d => d.UserName == chatMessage.Username).Points ?? 0}]");
+                twitchBot.Send(chatMessage.Channel, $"/me APU [ {database.Dicegamedbs.FirstOrDefault(r => r.UserName == chatMessage.Username).Rank} ] @{chatMessage.Username}, you got a {randDice} [current points: {database.Dicegamedbs.FirstOrDefault(d => d.UserName == chatMessage.Username).Points ?? 0}]");
                 DiceSaveTimer saveTimer = new(chatMessage.Channel, chatMessage.Username, twitchBot);
 
             }
             else
             {
-                twitchBot.Send(chatMessage.Channel, $"/me APU {$"[ {database.Dicegamedbs.FirstOrDefault(r => r.UserName == chatMessage.Username).Rank} ]"} @{chatMessage.Username}, you got a {randDice}");
-                _ = database.Dicegamedbs.Add(new Dicegamedb { UserName = chatMessage.Username, Points = randDice });
-                database.SaveChanges();
-                database.Dicegamedbs.FirstOrDefault(d => d.UserName == chatMessage.Username).PingMe = true;
+                Console.WriteLine("hello");
+                twitchBot.Send(chatMessage.Channel, $"/me APU @{chatMessage.Username}, you got a {randDice}");
+                _ = database.Dicegamedbs.Add(new Dicegamedb { UserName = chatMessage.Username, Points = randDice, PingMe = true, Rank = "-" });
                 database.SaveChanges();
                 DiceSaveTimer saveTimer = new(chatMessage.Channel, chatMessage.Username, twitchBot);
             }
